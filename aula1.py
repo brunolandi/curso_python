3965916b8910ffaebb79ed5d50d4c75601b9dcfc
Bruno Landi <brulandi@hotmail.com>
2024-11-17 02:13:39 -0300

    first commit

---
+++ aula1.py
@@ -0,0 +1,15 @@
+"""
+DocString
+triplo duplo aspas
+NÃO É COMENTÁRIO, MAS PODE SER UTILIZADO PARA COMENTAR, INTERPRETADOR IRÁ LER
+"""
+
+'''
+triplo aspas únicas
+NÃO É COMENTÁRIO, MAS FUNCIONA PARA TAL
+'''
+
+# Permite escrever um comentário
+print('Hello, World!') # Escrevendo Hello World
+print(123) # Escrevendo número completo
+print(1.2) # Escrevendo número real
