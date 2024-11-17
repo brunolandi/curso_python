ede38427b9301e703e1384cd6b160aceac5404c2
Bruno Landi <brulandi@hotmail.com>
2024-11-17 02:26:04 -0300

    nova aula

---
+++ aula2.py
@@ -0,0 +1,9 @@
+# \r\n -> CRLF quebra de linha
+# \n -> LF quebra de linha UNIX
+print(12, 34, 1011, 10, 11, sep='-', end='##\n') # quebra de linha em python por padrão é sensítivel pela escrita na IDE
+print(56, 78, sep="-", end='\n')
+print(9, 10, sep='*')
+
+
+#sep = separador
+#end = final da linha
